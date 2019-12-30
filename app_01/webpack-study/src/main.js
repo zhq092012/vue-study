df23880a4cfcadc6c763181ddc960e457f8bc171
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2019-12-30 22:31:57 +0800

    webpack的基本使用

--- app_01/webpack-study/src/main.js
+++ app_01/webpack-study/src/main.js
@@ -4,7 +4,11 @@
 import $ from 'jquery'
 $(function () {
   $('li:odd').css('backgroundColor', 'lightblue')
-  $('li:even').css('backgroundColor', function () {
-    return '#' + 'D097634'
-  })
+  $('li:even').css('backgroundColor', 'red')
 })
+//webpack 可以做什么？？？？
+/*
+  1.webpack能够处理js文件之间的依赖关系
+  2.webpack可以将浏览器不兼容的js语法处理掉，把高级js语法转为低级的浏览器能够识别的语法
+  3.webpack 要打包的文件路径 -0 输出文件的路径
+*/
